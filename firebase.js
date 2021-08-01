1c251ed8a2f3c4dcebdbc0ac1b5f8c8ce1dc8ea9
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-02 02:54:05 +0530

    Added Firebase Integration

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDwZblGxFmw5A578JLjtnhmlE3C8YpjejE",
+    authDomain: "fir-2e949.firebaseapp.com",
+    projectId: "fir-2e949",
+    storageBucket: "fir-2e949.appspot.com",
+    messagingSenderId: "259363328026",
+    appId: "1:259363328026:web:e98fed0a8bf5a3d63ac1a3",
+    measurementId: "G-3STQCVFZFR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
